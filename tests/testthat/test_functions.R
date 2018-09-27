545ff8078e06ae9d7e0a5037c572faa97df6a885
krzysztof-frappsilon <krzysztof@appsilon.com>
2018-09-27 12:09:27 +0200

    fixed one column not be a factor

--- tests/testthat/test_functions.R
+++ tests/testthat/test_functions.R
@@ -4,7 +4,8 @@ mock_data <- data.frame(
   va = c(1:10),
   vb = c(2:11),
   vc = factor(c(rep(1:2, 5)), labels = c("jeden", "dwa")),
-  vd = c("jeden", "dwa", "z", "z", "z", "z", "z", "z", "z", "o"))
+  vd = c(rep(c("jeden", "dwa", "x", "y", "z"), 2)),
+  stringsAsFactors = FALSE)
 
 test_that("sum_columns returns vector of correct length", {
   expect_length((sum_columns(mock_data)), 2)
@@ -16,5 +17,6 @@ test_that("return_class_specific_attr returns correct types", {
   expect_type(return_class_specific_attr(mock_data)[[1]], "integer")
   expect_type(return_class_specific_attr(mock_data)[[3]], "character")
   expect_true(is.factor(mock_data[[3]]))
+  expect_false(is.factor(mock_data[[4]]))
   expect_false(is.factor(mock_data[[1]]))
 })
