c31192a14efd54d067c3d8440b151ba5c8108631
krzysztof-frappsilon <krzysztof@appsilon.com>
2018-09-27 11:56:34 +0200

    extended mock data, added tests

--- tests/testthat/test_functions.R
+++ tests/testthat/test_functions.R
@@ -1,13 +1,20 @@
 context("Tests of package functions")
 
-mock_data <- data.table::data.table(va = c(1, 5, 4), vb = c(3, 7, 6), vc = c("one", "two", "three"))
+mock_data <- data.frame(
+  va = c(1:10),
+  vb = c(2:11),
+  vc = factor(c(rep(1:2, 5)), labels = c("jeden", "dwa")),
+  vd = c("jeden", "dwa", "z", "z", "z", "z", "z", "z", "z", "o"))
 
-testthat::test_that("sum_columns returns vector of correct length", {
-  testthat::expect_length((sum_columns(mock_data)), 2)
+test_that("sum_columns returns vector of correct length", {
+  expect_length((sum_columns(mock_data)), 2)
+  expect_equal(sum_columns(mock_data)[[1]], 55)
 })
 
-testthat::test_that("return_class_specific_attr returns correct types", {
-  testthat::expect_length(return_class_specific_attr(mock_data), 3)
-  testthat::expect_type(return_class_specific_attr(mock_data)[[1]], "double")
-  testthat::expect_type(return_class_specific_attr(mock_data)[[3]], "character")
+test_that("return_class_specific_attr returns correct types", {
+  expect_length(return_class_specific_attr(mock_data), 4)
+  expect_type(return_class_specific_attr(mock_data)[[1]], "integer")
+  expect_type(return_class_specific_attr(mock_data)[[3]], "character")
+  expect_true(is.factor(mock_data[[3]]))
+  expect_false(is.factor(mock_data[[1]]))
 })
