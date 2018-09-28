e577d2f60dfa1a4fc8bff62e74f05567b822f67f
krzysztof-frappsilon <krzysztof@appsilon.com>
2018-09-28 11:35:29 +0200

    changed names to english, fixed tests

--- tests/testthat/test_functions.R
+++ tests/testthat/test_functions.R
@@ -3,7 +3,7 @@ context("Tests of package functions")
 mock_data <- data.frame(
   va = c(1:10),
   vb = c(2:11),
-  vc = factor(c(rep(1:2, 5)), labels = c("jeden", "dwa")),
+  vc = factor(c(rep(1:2, 5)), labels = c("one", "two")),
   vd = c(rep(c("jeden", "dwa", "x", "y", "z"), 2)),
   stringsAsFactors = FALSE)
 
@@ -14,9 +14,12 @@ test_that("sum_columns returns vector of correct length", {
 
 test_that("return_class_specific_attr returns correct types", {
   expect_length(return_class_specific_attr(mock_data), 4)
+
   expect_type(return_class_specific_attr(mock_data)[[1]], "integer")
   expect_type(return_class_specific_attr(mock_data)[[3]], "character")
-  expect_true(is.factor(mock_data[[3]]))
-  expect_false(is.factor(mock_data[[4]]))
-  expect_false(is.factor(mock_data[[1]]))
+  expect_type(return_class_specific_attr(mock_data)[[4]], "character")
+
+  expect_type(unlist(return_class_specific_attr(mock_data)[[1]]), "integer")
+  expect_type(unlist(return_class_specific_attr(mock_data[[3]])), "integer")
+  expect_type(unlist(return_class_specific_attr(mock_data[[4]])), "character")
 })
