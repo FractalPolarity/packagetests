f7869b7fd2f171b4b74771cce45a161776666508
krzysztof-frappsilon <krzysztof@appsilon.com>
2018-10-02 09:14:21 +0200

    fixed code style

--- R/functions.R
+++ R/functions.R
@@ -1,16 +1,16 @@
 #' Sum numeric columns of input data
 #' @param data dataset on which to perform operation
 #' @export
-sum_columns <- function(data){
+sum_columns <- function(data) {
   dplyr::summarise_if(data, is.numeric, sum)
-  }
+}
 
 #' Display levels for factors, unique, for characters
 #' and range for numeric values
 #'@param data input dataset
 #'@export
 #'@rdname sum_columns
-return_class_specific_attr <- function(data){
+return_class_specific_attr <- function(data) {
   purrr::map_if(data, is.numeric, range) %>%
     purrr::map_if(is.character, unique) %>%
     purrr::map_if(is.factor, levels)
@@ -19,6 +19,6 @@ return_class_specific_attr <- function(data){
 #'Return columns of dataset
 #'@param data input dataset
 #'@export
-get_column_names <- function(data){
+get_column_names <- function(data) {
   colnames(data)
 }
